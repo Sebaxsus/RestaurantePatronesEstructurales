509a0993fba9be3a30d7f51c07439822cfe594e6
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-11-25 21:51:31 -0500

    Inicializando el proyecto y declarando su estructura

---
+++ descuento/DescuentoStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantePatronesEstructurales.descuento
+{
+    public interface DescuentoStrategy
+    {
+    }
+}
